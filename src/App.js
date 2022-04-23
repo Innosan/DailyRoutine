57aaa712ffc282dcb8f040f872a31aaffcea410b
InnoSan <reau_beua@gmail.com>
2022-04-23 16:02:28 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          kekw!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p className="LogoText">Daily Routine</p>
       </header>
     </div>
   );
